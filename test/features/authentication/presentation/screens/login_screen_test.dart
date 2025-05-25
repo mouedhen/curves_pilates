0156e014e0eedd73dfff881e149d390f18b7d890
Mohamed Chams Eddin Mouedhen <chams-med@hotmail.fr>
2025-05-25 19:04:06 +0100

    fix: Update import paths to reflect new package structure

--- test/features/authentication/presentation/screens/login_screen_test.dart
+++ test/features/authentication/presentation/screens/login_screen_test.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_foundations_01_task_manager_app/common/styling/app_colors.dart';
-import 'package:flutter_foundations_01_task_manager_app/features/authentication/presentation/screens/login_screen.dart';
-import 'package:flutter_foundations_01_task_manager_app/features/authentication/presentation/provider/auth_provider.dart';
-import 'package:flutter_foundations_01_task_manager_app/features/authentication/domain/use_cases/login_use_case.dart';
-import 'package:flutter_foundations_01_task_manager_app/features/authentication/data/repositories/auth_repository.dart';
-import 'package:flutter_foundations_01_task_manager_app/common/services/api_service.dart';
-import 'package:flutter_foundations_01_task_manager_app/common/widgets/loading_indicator.dart';
+import 'package:curves_pilates/common/styling/app_colors.dart';
+import 'package:curves_pilates/features/authentication/presentation/screens/login_screen.dart';
+import 'package:curves_pilates/features/authentication/presentation/provider/auth_provider.dart';
+import 'package:curves_pilates/features/authentication/domain/use_cases/login_use_case.dart';
+import 'package:curves_pilates/features/authentication/data/repositories/auth_repository.dart';
+import 'package:curves_pilates/common/services/api_service.dart';
+import 'package:curves_pilates/common/widgets/loading_indicator.dart';
 import 'package:mockito/mockito.dart'; // Required for mocking
 
 // Mock classes
